d21ea2b11b375dd861c76a80272129c0d295f8e8
liwuv587 <99467354+liwuv587@users.noreply.github.com>
2024-03-31 13:54:29 +0000

    feat lib.rs

--- esp/programs/esp/src/lib.rs
+++ esp/programs/esp/src/lib.rs
@@ -3,13 +3,33 @@ use anchor_lang::prelude::*;
 declare_id!("7dsPHAwPMArDiW4bX6n2s4C1S7qix2h7hfq2qZrPaGb6");
 
 #[program]
-pub mod esp {
+mod hello_anchor {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
+        if ctx.accounts.token_account.amount > 0 {
+            ctx.accounts.my_account.data = data;
+        }
         Ok(())
     }
 }
 
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+    data: u64,
+    mint: Pubkey
+}
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetData<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(
+        constraint = my_account.mint == token_account.mint,
+        has_one = owner
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub owner: Signer<'info>
+}
